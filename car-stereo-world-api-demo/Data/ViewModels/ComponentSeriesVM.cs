1035bad8717aedf040fd74efc6ecf3af1a60cce4
Magnus Oma <magnus.oma@gmail.com>
2021-07-03 19:19:58 +0200

    Component series list item view model

--- car-stereo-world-api-demo/Data/ViewModels/ComponentSeriesVM.cs
+++ car-stereo-world-api-demo/Data/ViewModels/ComponentSeriesVM.cs
@@ -13,6 +13,16 @@ namespace car_stereo_world_api_demo.Data.ViewModels
         public int BrandId { get; set; }
     }
 
+    public class ComponentSeriesListItemVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ProducedFrom { get; set; }
+        public DateTime? ProducedUntil { get; set; }
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+    }
+
     public class ComponentSeriesWithBrandVM
     {
         public int Id { get; set; }
